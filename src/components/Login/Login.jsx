87b443e852de10f4516ebd3fef9f6a7d204aca2c
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2026-01-16 13:21:07 +0600

    update login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../../firebase/firebase";
 import { useState } from "react";
 
@@ -11,20 +11,35 @@ const handleGoogleSignIn = () => {
         console.log(result.user);
         setUser(result.user);
     })
+
   .catch(error => {
     console.log('ERROR', error);
     setUser(null);
   })
- 
 }
+const handleSignOut = () =>{
+signOut(auth)
+.then(() =>{
+  console.log('sign out done');
+  setUser(null);
+})
+.catch(error =>
+  console.log(error))
+}
+
  return (
         <div>
-           <button onClick={handleGoogleSignIn}>Login with google</button> 
+           {/* <button onClick={handleGoogleSignIn}>Login with google</button> 
+       <button onClick={handleSignOut}>Sign Out</button> */}
+       {
+        user ?  <button onClick={handleSignOut}>Sign Out</button>
+        : <button onClick={handleGoogleSignIn}>Login with google</button> 
+       }
         {
          user && <div>
           <h4> {user.displayName} </h4>
           <p>Email:  {user.email} </p>
-       
+       <img src={user.photoURL} alt="" />
          </div>
         }
         </div>
