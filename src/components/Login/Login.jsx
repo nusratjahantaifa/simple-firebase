5f279f74c3c211805f7507cd62c63e80b50566ae
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2026-01-16 12:08:19 +0600

    my commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,32 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../firebase/firebase";
+import { useState } from "react";
 
 const Login = () => {
-
+const [user, setUser] = useState(null);
 const provider = new GoogleAuthProvider();
 const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
     .then(result =>{
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
     })
   .catch(error => {
     console.log('ERROR', error);
+    setUser(null);
   })
  
 }
  return (
         <div>
            <button onClick={handleGoogleSignIn}>Login with google</button> 
+        {
+         user && <div>
+          <h4> {user.displayName} </h4>
+          <p>Email:  {user.email} </p>
+       
+         </div>
+        }
         </div>
     );
 };
